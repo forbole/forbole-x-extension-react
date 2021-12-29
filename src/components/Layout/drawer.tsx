2b8090912cf75c3e6e197271ebc32879bf2343ae
Franco Chau <franco0506@gmail.com>
2021-12-29 18:47:55 +0800

    feat: add drawer component

---
+++ src/components/Layout/drawer.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+
+interface Props {
+  open: boolean;
+  setOpen: (status: boolean) => void;
+}
+
+const Drawer = ({open, setOpen}: Props) => {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={setOpen}>
+        <div className="absolute inset-0 overflow-hidden">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className='fixed inset-y-0 left-0 pr-10 max-w-full flex'>
+              <Transition.Child
+                as={Fragment}
+                enter='transform transition ease-in-out duration-500 sm:duration-700'
+                enterFrom='-translate-x-full'
+                enterTo='translate-x-0'
+                leave='transform transition ease-in-out duration-500 sm:duration-700'
+                leaveFrom='translate-x-0'
+                leaveTo='-translate-x-full z-index-0'
+              >
+              <div className="w-screen max-w-xs">
+                <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-auto">
+                  <div className="px-4 sm:px-6">
+                    <div className="flex items-start justify-between">
+                      <Dialog.Title className="text-lg font-medium text-gray-900">Panel title</Dialog.Title>
+                      <div className="ml-3 h-7 flex items-center">
+                        <button
+                          type="button"
+                          className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          onClick={() => setOpen(false)}
+                        >
+                          <span className="sr-only">Close panel</span>
+                         X
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-6 relative flex-1 px-4 sm:px-6">
+                    {/* Replace with your content */}
+                    <div className="absolute inset-0 px-4 sm:px-6">
+                      <div className="h-full border-2 border-dashed border-gray-200" aria-hidden="true" />
+                    </div>
+                    {/* /End replace */}
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default Drawer;
--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,29 +1,41 @@
-import React, { useEffect } from "react";
-import { ReactComponent as MenuIcon } from "../../assets/images/icons/icon_menu.svg";
-import { ReactComponent as WalletIcon } from "../../assets/images/icons/icon_wallet_manage.svg";
+import React, { useEffect } from 'react';
+import { ReactComponent as MenuIcon } from '../../assets/images/icons/icon_menu.svg';
+import { ReactComponent as WalletIcon } from '../../assets/images/icons/icon_wallet_manage.svg';
 // @ts-ignore
-import nightwind from "nightwind/helper";
+import nightwind from 'nightwind/helper';
+import Drawer from './drawer';
+import { useState } from 'react';
 
 interface Props {
   children: JSX.Element;
 }
 
 const Layout = ({ children }: Props) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="bg-backgroundColor-100 text-black w-full min-h-screen">
-      <div className="w-full flex justify-between items-center p-5">
-        <MenuIcon className="w-6 fill-dark dark:fill-white" />
-        <h4
-          onClick={() => {
-            nightwind.toggle();
-          }}
-        >
-          DSM
-        </h4>
-        <WalletIcon className="w-5 fill-dark dark:fill-white" />
+    <>
+      <div className='bg-backgroundColor-100 text-black w-full min-h-screen'>
+        <div className='w-full flex justify-between items-center p-5'>
+          <MenuIcon
+            className='w-6 fill-dark dark:fill-white cursor-pointer'
+            onClick={() => {
+              setOpen(!open);
+            }}
+          />
+          <h4
+            onClick={() => {
+              nightwind.toggle();
+            }}
+          >
+            DSM
+          </h4>
+          <WalletIcon className='w-5 fill-dark dark:fill-white' />
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+      <Drawer open={open} setOpen={setOpen} />
+    </>
   );
 };
 
