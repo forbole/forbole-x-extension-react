d32b2cb5d9e9f0d6615a5bb089e2119987d1a708
Franco Chau <franco0506@gmail.com>
2021-12-31 15:49:29 +0800

    fix: remove unused code

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import Layout from "./components/Layout/layout";
-
-function App() {
-  return <Layout><div>123</div></Layout>;
-}
-
-export default App;
