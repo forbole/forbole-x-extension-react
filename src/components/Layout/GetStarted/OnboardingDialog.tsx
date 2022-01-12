95d8491c9974b16aa1ada0f0c59bf34a3620d254
Franco Chau <franco0506@gmail.com>
2022-01-12 22:32:45 +0800

    feat: password form

--- src/components/Layout/GetStarted/OnboardingDialog.tsx
+++ src/components/Layout/GetStarted/OnboardingDialog.tsx
@@ -11,28 +11,18 @@ interface Props {
   onSubmit: (password: string) => void;
 }
 
+type Inputs = {
+  password: string;
+  confirmPassword: string;
+};
+
 const OnboardingDialog = ({ open, onClose, onSubmit }: Props) => {
   const [password, setPassword] = React.useState('');
   const [confirmPassword, setConfirmPassword] = React.useState('');
   const [isConfirmingPassword, setIsConfirmingPassword] = React.useState(false);
   const [error, setError] = React.useState('');
 
-  const [firstTime, setFirstTime] = useFirstTime()
-
-  // const onButtonClick = React.useCallback(() => {
-  //   setError('');
-  //   if (!isConfirmingPassword) {
-  //     if (password.length < 6) {
-  //       setError('invalid password');
-  //     } else {
-  //       setIsConfirmingPassword(true);
-  //     }
-  //   } else if (password !== confirmPassword) {
-  //     setError('invalid confirm password');
-  //   } else {
-  //     onSubmit(password);
-  //   }
-  // }, [isConfirmingPassword, password, confirmPassword]);
+  const [firstTime, setFirstTime] = useFirstTime();
 
   React.useEffect(() => {
     if (open) {
@@ -46,8 +36,10 @@ const OnboardingDialog = ({ open, onClose, onSubmit }: Props) => {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { errors },
-  } = useForm();
+  } = useForm<Inputs>();
+
   const onFormSubmit = (data) => {
     console.log(data, isConfirmingPassword);
     if (!isConfirmingPassword) {
@@ -56,44 +48,68 @@ const OnboardingDialog = ({ open, onClose, onSubmit }: Props) => {
       } else {
         setIsConfirmingPassword(true);
       }
-    } else if (password !== confirmPassword) {
+    } else if (watch('password') !== watch('confirmPassword')) {
       setError('invalid confirm password');
     } else {
-      onSubmit(password);
-      setFirstTime(false);
+      onSubmit(watch('password'));
+      // setFirstTime(false);
+      setError('');
+      reset({ password: '', confirmPassword: '' });
     }
   };
 
   return (
-    <Dialog open={open} onClose={() => onClose(false)} title='Set Password'>
+    <Dialog
+      open={open}
+      onClose={() => {
+        onClose(false);
+        setError('');
+        reset({ password: '', confirmPassword: '' });
+      }}
+      title={`${
+        !isConfirmingPassword ? 'Set Wallet Password' : 'Confirm Password'
+      }`}
+    >
       <form onSubmit={handleSubmit(onFormSubmit)}>
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center mt-5'>
           <p className='max-w-sm text-center'>
-            Set a password to prevent unauthorized access to wallet data on your
-            application
+            {!isConfirmingPassword
+              ? 'Set a password to prevent unauthorized access to wallet data on your application'
+              : 'There will be No “restore password” button. Make sure you remember it.'}
           </p>
           <div className='w-full flex justify-center'>
             <div className='w-full px-10 py-7'>
               {!isConfirmingPassword ? (
                 <input
+                  key='password'
                   type='password'
                   {...register('password', { required: true })}
                   className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full text-sm border-gray-300 rounded-sm px-3 py-2 bg-popup-100'
                 />
               ) : (
                 <input
+                  key='confirmPassword'
                   type='password'
                   {...register('confirmPassword', { required: true })}
                   className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full text-sm border-gray-300 rounded-sm px-3 py-2 bg-popup-100'
                 />
               )}
-              {watch(confirmPassword)}
-              {/* <Input /> */}
-              <p className='text-sm mt-2'>
-                * at least 6 characters in length {password}
-              </p>
+              {!!error && (
+                <p className='text-sm mt-2 text-red-500 nightwind-prevent'>
+                  {error}
+                </p>
+              )}
+                <p className='text-sm mt-2'>
+                  {!isConfirmingPassword
+                    ? '* at least 6 characters in length'
+                    : '* The password will be saved on your application, we won’t be able to help if you lose your password.'}
+                </p>
               <div className='mt-24'>
-                <Button text='Next' type='submit' />
+                {/* TODO: Previous Step */}
+                <Button
+                  text={`${!isConfirmingPassword ? 'Next' : 'Confirm'}`}
+                  type='submit'
+                />
               </div>
             </div>
           </div>
---
+++ src/components/Layout/GetStarted/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import usePersistedState from '../../../misc/usePersistedState';
+import OnboardingDialog from './OnboardingDialog';
+
+import WelcomeStep from './WelcomeStep';
+import CreateWalletDialog from './CreateWalletDialog';
+import { useState } from 'react';
+import Button from '../../Element/button';
+
+interface Props {}
+
+const GetStarted = (props: Props) => {
+  const [isOnboardingDialogOpen, setIsOnboardingDialogOpen] = useState(false);
+  const [isCreateWalletDialogOpen, setIsCreateWalletDialogOpen] =
+    useState(false);
+
+  return (
+    <div className=''>
+      <WelcomeStep />
+      <div className='flex justify-center'>
+        <div className='pt-10 max-w-sm w-full'>
+          <Button text='Get Started' onClick={() => {
+              setIsOnboardingDialogOpen(true);
+            }} />
+        </div>
+      </div>
+
+      <OnboardingDialog
+        open={isOnboardingDialogOpen}
+        onClose={() => setIsOnboardingDialogOpen(false)}
+        onSubmit={(password) => {
+          setIsOnboardingDialogOpen(false);
+          setIsCreateWalletDialogOpen(true);
+        }}
+      />
+      <CreateWalletDialog
+        open={isCreateWalletDialogOpen}
+        onClose={() => setIsCreateWalletDialogOpen(false)}
+      />
+    </div>
+  );
+};
+
+export default GetStarted;
