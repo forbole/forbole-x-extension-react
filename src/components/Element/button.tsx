4944b9fac82746d6f9e528606b615af7efe0bbe0
Franco Chau <franco0506@gmail.com>
2022-01-20 03:27:34 +0800

    feat: common components

--- src/components/Element/button.tsx
+++ src/components/Element/button.tsx
@@ -4,14 +4,15 @@ interface Props {
   text?: string;
   onClick?: (x: any) => void;
   type?: any;
+  bgColor?: string;
 }
 
-const Button = ({ text, onClick, type }: Props) => {
+const Button = ({ text, onClick, type, bgColor = 'indigo' }: Props) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className='nightwind-prevent inline-flex items-center w-full justify-center py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+      className={`bg-${bgColor}-600 hover:bg-${bgColor}-700 focus:outline-none nightwind-prevent inline-flex items-center w-full justify-center py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white`}
     >
       {text}
     </button>
--- src/components/Element/buttonArea.tsx
+++ src/components/Element/buttonArea.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 interface Props {
-    children: JSX.Element;   
+    children: JSX.Element;  
+    onClick?: React.MouseEventHandler
 }
 
-const ButtonArea = ({children}: Props) => {
+const ButtonArea = ({children, onClick}: Props) => {
     return (
-        <div className='border hover:border-gray-500 rounded-sm cursor-pointer'>
+        <div className='border hover:border-gray-500 rounded-sm cursor-pointer' onClick={onClick}>
             {children}  
         </div>
     )
