16a4ed0cbed17546f27cc1e99d055a56a871104f
Franco Chau <franco0506@gmail.com>
2022-02-15 20:21:34 +0800

    feat: add password strength component

---
+++ src/components/CreateWallet/PasswordStrengthDisplay.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+type Props = {
+  passwordSecurityLevel: number;
+};
+
+const PasswordStrengthDisplay = ({ passwordSecurityLevel }: Props) => {
+  const indicator = [
+    { color: 'red-500', message: 'Weak' },
+    { color: 'yellow-700', message: 'Medium' },
+    { color: 'green-700', message: 'Strong' },
+    { color: 'green-700', message: 'Strong' },
+  ];
+
+  return (
+    <div className='flex w-full h-1 items-center'>
+      <div
+        className={`nightwind-prevent bg-${indicator[passwordSecurityLevel].color}  w-full h-1`}
+      />
+      <div
+        className={`nightwind-prevent bg-${
+          passwordSecurityLevel > 0
+            ? indicator[passwordSecurityLevel].color
+            : 'gray-800'
+        }  w-full h-1`}
+      />
+      <div
+        className={`nightwind-prevent bg-${
+          passwordSecurityLevel > 1
+            ? indicator[passwordSecurityLevel].color
+            : 'gray-800'
+        }  w-full h-1`}
+      />
+      <p
+        className={`ml-3 text-sm nightwind-prevent text-${indicator[passwordSecurityLevel].color}`}
+      >
+        {indicator[passwordSecurityLevel].message}
+      </p>
+    </div>
+  );
+};
+
+export default PasswordStrengthDisplay;
