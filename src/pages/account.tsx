9ad1903c802cf98beb557abf0513801de4677018
Franco Chau <franco0506@gmail.com>
2022-03-21 17:30:29 +0800

    feat: add balance card logic

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useRecoilValue, useRecoilValueLoadable } from 'recoil'
 import Layout from '../components/Layout/layout'
 import BalanceCard from '../components/Wallet/BalanceCard'
+import { accountDetailState } from '../recoil/accounts'
+import { isFirstTimeUserState, passwordState } from '../recoil/general'
+import { currentWalletState } from '../recoil/wallets'
 
 type Props = {}
 
 const Account = (props: Props) => {
+  const params = useParams();
+
+
+  const firstTime = useRecoilValueLoadable(isFirstTimeUserState)
+  const password = useRecoilValue(passwordState)
+  const wallet = useRecoilValueLoadable(currentWalletState)
+  const account = useRecoilValueLoadable(accountDetailState({ walletId: wallet.contents?.id, address: params.address }))
+
   return (
     <Layout title="Account" backPath="/">
-      <BalanceCard />
+      <BalanceCard account={account} />
     </Layout>
   )
 }
