19af840d23901020c1a0141e52fab8efef3bede0
Franco Chau <franco0506@gmail.com>
2022-04-01 16:40:00 +0800

    fix: dynamic avatar and theme

--- src/components/Wallet/WalletCard.tsx
+++ src/components/Wallet/WalletCard.tsx
@@ -4,7 +4,8 @@ import { ReactComponent as IconCopy } from '../../assets/images/icons/icon_copy.
 import { ReactComponent as IconQrcode } from '../../assets/images/icons/icon_qrcode.svg'
 import { ReactComponent as IconMore } from '../../assets/images/icons/icon_more.svg'
 import { Loadable } from 'recoil'
-import toast from 'react-hot-toast';
+import chains from '../../misc/chains'
+import toast from 'react-hot-toast'
 
 type Props = {
   account: Loadable<AccountDetail>
@@ -23,26 +24,35 @@ const WalletCard = ({ account }: Props) => {
     [account.contents.address]
   )
 
-
   return (
     <div className="mx-5 p-6 rounded-xl bg-popup-100">
       <div className="w-full flex items-start space-x-3 mb-3">
-        <DsmAvatar className="w-10 h-10 self-center" />
-        <div className='w-full'>
+        <img
+          src={chains[account.contents.chain]?.image}
+          className="w-9 h-9 self-center"
+          alt={account.contents.chain}
+        />
+        <div className="w-full">
           <h4>{account.contents.name}</h4>
           <div className="flex items-center space-x-1">
             <span className="text-font-200 text-xs leading-none">{account.contents.address}</span>
-            <IconCopy onClick={copyText} className='cursor-pointer' />
-            <IconQrcode className='cursor-pointer' />
+            <IconCopy onClick={copyText} className="cursor-pointer" />
+            <IconQrcode className="cursor-pointer" />
           </div>
         </div>
-        <IconMore className='cursor-pointer' />
+        <IconMore className="cursor-pointer" />
       </div>
-      <div className='flex justify-between space-x-4'>
-            <button className='nightwind-prevent text-white bg-primary-100 w-full py-[9px] rounded-md'>Delegate</button>
-            <button className='nightwind-prevent text-white bg-[#1EC490] w-full py-[9px] rounded-md'>Send</button>
-            <button className='nightwind-prevent text-white bg-[#ECB140] w-full py-[9px] rounded-md'>Withdraw Rewards</button>
-          </div>
+      <div className="flex justify-between space-x-4">
+        <button className="nightwind-prevent text-white bg-primary-100 w-full py-[9px] rounded-md">
+          Delegate
+        </button>
+        <button className="nightwind-prevent text-white bg-success-100 w-full py-[9px] rounded-md">
+          Send
+        </button>
+        <button className="nightwind-prevent text-white bg-secondary-100 w-full py-[9px] rounded-md">
+          Withdraw Rewards
+        </button>
+      </div>
     </div>
   )
 }
