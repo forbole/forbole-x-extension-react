153ff74bd346feacf96825598a5ef8d8511004ab
Kevin Pang <moistbobo@gmail.com>
2022-06-15 11:42:37 +0800

    chore: remove DUMMY_TX

--- src/recoil/transaction.ts
+++ src/recoil/transaction.ts
@@ -1,37 +1,5 @@
 import { atom } from 'recoil';
 
-const DUMMY_TX = {
-  address: 'desmos1lhr4w3yurtnqsdqyulfrcs042dku0aetvgh6tz',
-  chainID: 'desmos-mainnet-1',
-  transactionData: {
-    memo: 'default memo',
-    msgs: [
-      {
-        typeUrl: '/cosmos.staking.v1beta1.MsgDelegate',
-        value: {
-          delegatorAddress: 'desmos1lhr4w3yurtnqsdqyulfrcs042dku0aetvgh6tz',
-          validatorAddress: 'desmosvaloper17ue85ck027c4grv7nuks7k7p4fqnlc55uqhskj',
-          amount: {
-            amount: '1000000',
-            denom: 'udsm',
-          },
-        },
-      },
-      {
-        typeUrl: '/cosmos.staking.v1beta1.MsgDelegate',
-        value: {
-          delegatorAddress: 'desmos1lhr4w3yurtnqsdqyulfrcs042dku0aetvgh6tz',
-          validatorAddress: 'desmosvaloper1gwr9l765vfxv4l4zz8glsxwkkphj2084xjwc68',
-          amount: {
-            amount: '1000000',
-            denom: 'udsm',
-          },
-        },
-      },
-    ],
-  },
-};
-
 export type TransactionStateType = {
   /**
    * The address of the account making the transaction
@@ -51,13 +19,12 @@ export type TransactionStateType = {
 
 export const transactionState = atom<TransactionStateType>({
   key: 'transactionState',
-  default: DUMMY_TX as any,
-  // default: {
-  //   address: '',
-  //   chainID: '',
-  //   transactionData: {
-  //     memo: '',
-  //     msgs: [],
-  //   },
-  // },
+  default: {
+    address: '',
+    chainID: '',
+    transactionData: {
+      memo: '',
+      msgs: [],
+    },
+  },
 });
